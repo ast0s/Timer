4f387df97bfd6eda11febd65a89a7d775bd093de
BulatNick <bulatnikov.mykyta@vu.cdu.edu.ua>
2022-04-18 11:26:21 +0300

    Добавьте файлы проекта.

---
+++ Timer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько секунд вы хотите бесцельно прождать?");
+
+            Timer timer = new Timer(int.Parse(Console.ReadLine()));
+            TimerMessage tm = timer.ShowMessage;
+
+            int counter = 0;
+            while (true)
+            {
+                timer.Tick();
+                counter++;
+
+                if (timer.seconds == counter)
+                    tm.Invoke();
+            }
+        }
+        public delegate void TimerMessage();
+    }
+}
---
+++ Timer/Timer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    class Timer
+    {
+        public int seconds;
+        public Timer(int seconds) { this.seconds = seconds; }
+        public void Tick() => Thread.Sleep(999);
+        public void ShowMessage() => Console.WriteLine($"Пройшло {seconds} секунд");
+    }
+}
